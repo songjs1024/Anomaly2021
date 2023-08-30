ef09d62785b72c39d443fcaa763f49fee3b5dfdc
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-08-30 19:44:25 +0900

    Create data_generate.py

---
+++ data_handler/data_generate.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+class generater(object):
+  
+  def __init__(self,raw_path):
+    self.raw_path = raw_path
+    self.combine_data = data
+    
+
+  def combine(self):
+    data_List = os.listdir(self.raw_path)
+    for i in range(len(data_List):
+      tmp_raw = pd.read_excel(raw_path, engine ='openpyxl')
+      
+      
+      
+
+    
