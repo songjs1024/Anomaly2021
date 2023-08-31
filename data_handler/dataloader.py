38dfa4ffdbe3c8850a55667e25cdb24372875b7e
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-08-31 13:05:29 +0900

    Update dataloader.py

--- data_handler/dataloader.py
+++ data_handler/dataloader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-class data_loader(object):
+class KrugerLoader(object):
 
     def __init__(self, data_path, win_size, step, mode="train"):
         self.mode = mode
@@ -31,22 +31,51 @@ class data_loader(object):
         
         '''
 
-
-    def get_loader_segment(data_path, batch_size, win_size=1000, step=100, mode='train', dataset='vibx'):
-        if (dataset == 'kruger'):
-            dataset = SMDSegLoader(data_path, win_size, step, mode)
-        elif (dataset == 'punker'):
-            dataset = MSLSegLoader(data_path, win_size, 1, mode)
-
-        shuffle = False
-        if mode == 'train':
-            shuffle = True
+class PunkerLoader(object):
+    def __init__(self, data_path, win_size, step, mode="train"):
+        self.mode = mode
+        self.step = step
+        self.win_size = win_size
+        self.scaler = StandardScaler()
+        data = pd.read_csv(data_path + '/data.csv')
+        data = data.values[:, 1:]
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+    def __len__(self):
+        if self.mode == "train":
+            return (self.train.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'val'):
+            return (self.val.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'test'):
+            return (self.test.shape[0] - self.win_size) // self.step + 1
+        else:
+            return (self.test.shape[0] - self.win_size) // self.win_size + 1
     
-        data_loader = DataLoader(dataset=dataset,
-                                 batch_size=batch_size,
-                                 shuffle=shuffle,
-                                 num_workers=0)
-        return data_loader
+
+    def __get   (self):
+        '''
+        인덱스 메인에서 불러오는거 확인
+        
+        '''
+    
+    
+    
+    
+def get_loader_segment(data_path, batch_size, win_size=1000, step=100, mode='train', dataset='vibx'):
+    if (dataset == 'kruger'):
+        dataset = SMDSegLoader(data_path, win_size, step, mode)
+    elif (dataset == 'punker'):
+        dataset = MSLSegLoader(data_path, win_size, 1, mode)
+
+    shuffle = False
+    if mode == 'train':
+        shuffle = True
+        
+    data_loader = DataLoader(dataset=dataset,
+                             batch_size=batch_size,
+                             shuffle=shuffle,
+                             num_workers=0)
+    return data_loader
 
 
         
