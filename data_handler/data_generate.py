51eede1528ea4c1ded91e091685f5933c7a0e053
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-08-31 12:57:09 +0900

    Update data_generate.py

--- data_handler/data_generate.py
+++ data_handler/data_generate.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import glob
 
 class generater(object):
   
@@ -10,15 +11,15 @@ class generater(object):
 
   def combine(self):
     appended_data = []
-    for file in glob.glob(raw_path+'*.csv'):
-      tmp_raw = pd.read_csv(file,skiprows=8,usecols=['12000'])
-      appended_data.append(tmp_raw)
+    for file in glob.glob(self.raw_path+'*.csv'):
+        tmp_raw = pd.read_csv(file,skiprows=8,usecols=['12000'])
+        appended_data.append(tmp_raw)
+    
     appended_data = pd.concat(appended_data)
-    if not os.path.exists(raw_path+'train_data'): 
-      os.mkdir(raw_path+'train_data')
-    appended_data.to_csv(raw_path+'train_data'+'/train_data.csv',sep=',')    
+    if not os.path.exists(self.raw_path+'train_data'): 
+        os.mkdir(self.raw_path+'train_data')
+    appended_data.to_csv(self.raw_path+'train_data'+'/train_data.csv',sep=',')    
 
-      
       
 
     
