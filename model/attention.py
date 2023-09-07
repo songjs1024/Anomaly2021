73b6b826fd5899680070b6c951c313af792ac49a
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-07 20:16:32 +0900

    Update attention.py

--- model/attention.py
+++ model/attention.py
@@ -1,3 +1,7 @@
+import os
+import numpy as np
+import math
+from math import sqrt
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers as ln
@@ -9,17 +13,22 @@ from keras import utils
 
 class TriangularCausalMask():
     def __init__(self, B, L, device="cpu"):
+        if device == 'cpu':
+            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "0"
         mask_shape = [B, 1, L, L]
         with tf.stop_gradient():
-            self._mask = torch.triu(tf.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
+            ones = tf.ones_like(x)
+            slice_y_greater_than_one = tf.boolean_mask(ones, mask_shape)
+            self._mask = tf.linalg.band_part(slice_y_greater_than_one, -1,0)
+
+    @property
+    def mask(self):
+        return self._mask
+    
 '''
-ones = tf.ones_like(x) # create a tensor all ones
-mask = tf.greater(x, ones) # boolean tensor, mask[i] = True iff x[i] > 1
-slice_y_greater_than_one = tf.boolean_mask(y, mask)
-
-or 
-
-
+*test
 x = tf.constant([1, 2, 0, 4])
 y = tf.Variable([1, 2, 0, 4])
 mask = x > 1
@@ -30,11 +39,6 @@ with tf.Session() as sess:
     print (sess.run(slice_y_greater_than_one)) # [2 4]
 '''
 
-
-    @property
-    def mask(self):
-        return self._mask
-    
 
 class AnomalyAttention(utils.Sequence):
     def __init__(self, win_size, mask_flag=True, scale=None, attention_dropout=0.0, output_attention=False):
@@ -48,3 +52,32 @@ class AnomalyAttention(utils.Sequence):
         for i in range(window_size):
             for j in range(window_size):
                 self.distances[i][j] = abs(i - j)
+      
+    def forward(self, queries, keys, values, sigma, attn_mask):
+        B, L, H, E = queries.shape
+        _, S, _, D = values.shape
+        scale = self.scale or 1. / sqrt(E)
+    #https://baekyeongmin.github.io/dev/einsum/
+    #https://pytorch.org/docs/stable/generated/torch.einsum.html
+        scores = torch.einsum("blhe,bshe->bhls", queries, keys)
+        if self.mask_flag:
+            if attn_mask is None:
+                attn_mask = TriangularCausalMask(B, L, device=queries.device)
+            scores.masked_fill_(attn_mask.mask, -np.inf)
+        attn = scale * scores
+
+        sigma = sigma.transpose(1, 2)  # B L H ->  B H L
+        window_size = attn.shape[-1]
+        sigma = tf.math.sigmoid(sigma * 5) + 1e-5
+        sigma = tf.math.pow(3, sigma) - 1
+        sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L
+        prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda()
+        prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * tf.math.exp(-prior ** 2 / 2 / (sigma ** 2))
+
+        series = self.dropout(tf.nn.softmax(attn, dim=-1))#11
+        V = torch.einsum("bhls,bshd->blhd", series, values)
+
+        if self.output_attention:
+            return (V.contiguous(), series, prior, sigma)
+        else:
+            return (V.contiguous(), None)
