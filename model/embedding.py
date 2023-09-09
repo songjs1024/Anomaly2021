0a0db6184631fe4f2938ab778591f4fd8256eff0
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-09 10:14:14 +0900

    Update embedding.py

--- model/embedding.py
+++ model/embedding.py
@@ -1,6 +1,8 @@
 import math
 import tensorflow as tf
+from tensorflow import keras
 from keras import utils
+from keras import layers as ln
 
 class PositionalEmbedding(utils.Sequence):
     def __init__(self, d_model, max_len=5000):
@@ -20,3 +22,20 @@ class PositionalEmbedding(utils.Sequence):
 
     def forward(self, x):
         return self.pe[:, :x.size(1)]
+
+
+
+
+
+class DataEmbedding(utils.Sequence):
+    def __init__(self, c_in, d_model, dropout=0.0):
+        super(DataEmbedding, self).__init__()
+
+        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
+        self.position_embedding = PositionalEmbedding(d_model=d_model)
+
+        self.dropout = ln.Dropout(p=dropout)
+
+    def forward(self, x):
+        x = self.value_embedding(x) + self.position_embedding(x)
+        return self.dropout(x)
