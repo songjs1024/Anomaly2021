8808126542325907fa0c280947c346a71debab15
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-09 10:10:48 +0900

    Update embedding.py

---
+++ model/embedding.py
@@ -0,0 +1,22 @@
+import math
+import tensorflow as tf
+from keras import utils
+
+class PositionalEmbedding(utils.Sequence):
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEmbedding, self).__init__()
+        # Compute the positional encodings once in log space.
+        pe = tf.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = tf.range(0, max_len).float().unsqueeze(1)
+        div_term = (tf.range(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = tf.math.sin(position * div_term)
+        pe[:, 1::2] = tf.math.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
