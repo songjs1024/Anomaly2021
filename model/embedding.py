76c36cd3b0b608e63126c628bc30aaa30972024d
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-09 21:01:26 +0900

    Update embedding.py

--- model/embedding.py
+++ model/embedding.py
@@ -24,7 +24,29 @@ class PositionalEmbedding(utils.Sequence):
         return self.pe[:, :x.size(1)]
 
 
+class TokenEmbedding(utils.Sequence):
+    def __init__(self, c_in, d_model,x):
+        super(TokenEmbedding, self).__init__()
+        #tensorflow not use version 1.x  if tensorflow.__version__ >= 2.x  padding = 1 
+        #torch 1 if torch.__version__ >= '1.5.0' else 2
+        input_length  = c_in[0]
+        padding_size = 1
+        padded_input_data = tf.pad(x, [[padding_size, padding_size]])
+        filter_data = tf.constant([0.5, 1.0, 0.5], dtype=tf.float32)
+        padded_input_length = input_length + 2 * padding_size
+        padded_input_data = tf.reshape(padded_input_data, [1, padded_input_length, 1])
+        filter_data = tf.reshape(filter_data, [filter_data.shape[0], 1, 1])
+        self.tokenConv = ln.conv1d(padded_input_data, filter_data,input_shape=c_in, stride=1, padding='VALID',
+                                    stride=1,kernel_size=3,use_bias=False)
+       
+       #module cshnge
+        for m in self.modules():
+            if isinstance(m, ln.Conv1d):
+                tf.compat.v1.keras.initializers.glorot_uniform(seed=None,dtype=tf.dtypes.float32)
 
+    def forward(self, x):
+        x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
+        return x
 
 
 class DataEmbedding(utils.Sequence):
@@ -39,3 +61,5 @@ class DataEmbedding(utils.Sequence):
     def forward(self, x):
         x = self.value_embedding(x) + self.position_embedding(x)
         return self.dropout(x)
+
+
