2baa537aa6ebcf83c5b82b9b6d5dbcf04276581f
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-11 11:20:29 +0900

    Update solver.py

--- solver.py
+++ solver.py
@@ -96,5 +96,76 @@ class Solver(object):
   return np.average(loss_1), np.average(loss_2)  
 
 
+    def train(self):
+        time_now = time.time()
+        path = self.model_save_path
+        if not os.path.exists(path):
+            os.makedirs(path)
+        early_stopping = EarlyStopping(patience=3, verbose=True, dataset_name=self.dataset)
+        train_steps = len(self.train_loader)
+        for epoch in range(self.num_epochs):
+            iter_count = 0
+            loss1_list = []
 
+            epoch_time = time.time()
+            self.model.train()
+            for i, (input_data, labels) in enumerate(self.train_loader):
+
+                self.optimizer.zero_grad()
+                iter_count += 1
+                input = input_data.float().to(self.device)
+
+                output, series, prior, _ = self.model(input)
+
+                # calculate Association discrepancy
+                series_loss = 0.0
+                prior_loss = 0.0
+                for u in range(len(prior)):
+                    series_loss += (tf.reduce_mean(my_kl_loss(series[u], (
+                            prior[u] /tf.expand_dims(tf.reduce_sum(prior[u], -1), =-1).repeat(1, 1, 1,
+                                                                                                   self.win_size)).detach())) + tf.reduce_mean(
+                        my_kl_loss((prior[u] /tf.expand_dims(tf.reduce_sum(prior[u],-1),-1).repeat(1, 1, 1,
+                                                                                                           self.win_size)).detach(),
+                                   series[u])))
+                    prior_loss += (tf.reduce_mean(my_kl_loss(
+                        (prior[u] /tf.expand_dims(tf.reduce_sum(prior[u],=-1), -1).repeat(1, 1, 1,
+                                                                                                self.win_size)),
+                        series[u].detach())) + tf.reduce_mean(
+                        my_kl_loss(series[u].detach(), (
+                                prior[u] /tf.expand_dims(tf.reduce_sum(prior[u], -1),-1).repeat(1, 1, 1,
+                                                                                                       self.win_size)))))
+                series_loss = series_loss / len(prior)
+                prior_loss = prior_loss / len(prior)
+
+                rec_loss = self.criterion(output, input)
+
+                loss1_list.append((rec_loss - self.k * series_loss).item())
+                loss1 = rec_loss - self.k * series_loss
+                loss2 = rec_loss + self.k * prior_loss
+
+                if (i + 1) % 100 == 0:
+                    speed = (time.time() - time_now) / iter_count
+                    left_time = speed * ((self.num_epochs - epoch) * train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+
+                # Minimax strategy
+                loss1.backward(retain_graph=True)
+                loss2.backward()
+                self.optimizer.step()
+
+            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+            train_loss = np.average(loss1_list)
+
+            vali_loss1, vali_loss2 = self.vali(self.test_loader)
+
+            print(
+                "Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} ".format(
+                    epoch + 1, train_steps, train_loss, vali_loss1))
+            early_stopping(vali_loss1, vali_loss2, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+            adjust_learning_rate(self.optimizer, epoch + 1, self.lr)
 
