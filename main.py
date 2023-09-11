d11b6199ca34dc3a619be9b7115c3bc3608babb4
songjs1024 <33867367+songjs1024@users.noreply.github.com>
2023-09-11 10:57:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 import os
 import argparse
+import tensorflow as tf
+from utils.utils import *
+
+from solver import Solver
 
 
+def str2bool(v):
+    return v.lower() in ('true')
 
 
+def main(config):
+    tf.test.is_built_with_cuda()
+
+    if (not os.path.exists(config.model_save_path)):
+        os.mkdir(config.model_save_path)
+    solver = Solver(vars(config))
+
+    if config.mode == 'train':
+        solver.train()
+    elif config.mode == 'test':
+        solver.test()
+
+    return solver
 
 
 if __name__ == '__main__':
-    print('------------ Options -------------')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, default='./dataset/creditcard_ts.csv')
+
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--k', type=int, default=3)
+    parser.add_argument('--win_size', type=int, default=100)
+    parser.add_argument('--input_c', type=int, default=38)
+    parser.add_argument('--output_c', type=int, default=38)
+    parser.add_argument('--batch_size', type=int, default=1024)
+    parser.add_argument('--pretrained_model', type=str, default=None)
+    parser.add_argument('--dataset', type=str, default='vib_dataset')
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    parser.add_argument('--data_path', type=str, default='./dataset/concat_data.csv')
     parser.add_argument('--model_save_path', type=str, default='checkpoints')
+    parser.add_argument('--anormly_ratio', type=float, default=4.00)
+
+    config = parser.parse_args()
+
+    args = vars(config)
+    print('------------ Options -------------')
+    for k, v in sorted(args.items()):
+        print('%s: %s' % (str(k), str(v)))
+    print('-------------- End ----------------')
+    main(config)
